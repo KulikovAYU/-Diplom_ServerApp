79a014a191ac1ee5e6db41c5ad6bda8681800573
Anton <tosha37@inbox.ru>
2019-03-03 22:52:27 +0300

    ServerPartStart. AddManual to connect and debugging at localhost from Phone, Genymation and android

---
+++ ServerApp/FC_EMDB/Training.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FC_EMDB
+{
+
+    public class Training
+    {
+        public int mnTrtainingId { get; set; } //id тренировки
+
+        public DateTime mStartTime { get; set; }//время начала
+
+        public DateTime mEndDime { get; set; }//время окончания тренировки
+
+        public string mTrainingName { get; set; }//название тренировки
+
+        public bool mbIsFinished { get; set; }//признак законченной тренировки
+
+        public string mGymName { get; set; } //название зала
+
+        public string mLevelName { get; set; }//уровень
+
+        public string mCoachName { get; set; }//имя инструктора
+
+        public string mDescription { get; set; }// описание
+
+        public bool mbIsReplaced { get; set; }//заменена ли тренировка
+
+        public bool mbIsMustPay { get; set; }//платная
+
+        public string mProgramType { get; set; }//тип программы
+
+        public bool mbIspopular { get; set; }//признак популярности
+
+        public int mnplacesCount { get; set; }//количество записей (вместимость)
+
+        public int mnFreePlacesCount { get; set; }//количество свободных мест
+
+        public int mnBusyPlacesCount { get; set; }// количество занятых мест
+
+    }
+
+}
