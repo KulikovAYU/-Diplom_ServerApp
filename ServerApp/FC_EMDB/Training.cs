6b13961072f0751c49b108aa59ed64f3107a010f
Anton <tosha37@inbox.ru>
2019-03-08 00:28:40 +0300

    adding Builder

--- ServerApp/FC_EMDB/Training.cs
+++ ServerApp/FC_EMDB/Training.cs
@@ -5,6 +5,28 @@ namespace FC_EMDB
 
     public class Training
     {
+        
+
+        public Training(TrainingBuilder trainingBuilder)
+        {
+            mStartTime = trainingBuilder.GetStartTime();
+            mEndDime = trainingBuilder.GetEndTime();
+            mTrainingName = trainingBuilder.GetTrainingName();
+            mbIsFinished = trainingBuilder.GetIsFinished();
+            mGymName = trainingBuilder.GetGymName();
+            mLevelName = trainingBuilder.GetLevelName();
+            mCoachName = trainingBuilder.GetCoachFullname();
+            mDescription = trainingBuilder.GetDescription();
+            mbIsReplaced = trainingBuilder.GetIsReplacedStatus();
+            mbIsNewTraining = trainingBuilder.GetIsNewTraining();
+            mEndDime = trainingBuilder.GetEndTime();
+            mProgramType = trainingBuilder.GetProgramType();
+            mbIsFinished = trainingBuilder.GetIsFinished();
+            mbIspopular = trainingBuilder.GetIsPopular();
+            if (mbIsMustPay)
+                mTrainingName += " (платная секция) по записи";
+        }
+
         public int mnTrtainingId { get; set; } //id тренировки
 
         public DateTime mStartTime { get; set; }//время начала
@@ -21,6 +43,8 @@ namespace FC_EMDB
 
         public string mCoachName { get; set; }//имя инструктора
 
+        public string mCoachFamily { get; set; }//фамилия инструктора
+
         public string mDescription { get; set; }// описание
 
         public bool mbIsReplaced { get; set; }//заменена ли тренировка
@@ -28,6 +52,8 @@ namespace FC_EMDB
         public bool mbIsMustPay { get; set; }//платная
 
         public string mProgramType { get; set; }//тип программы
+
+        public bool mbIsNewTraining { get; set; } //новая тренировка
 
         public bool mbIspopular { get; set; }//признак популярности
 
