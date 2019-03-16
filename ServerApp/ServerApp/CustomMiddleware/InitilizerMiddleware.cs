3d74f1ddc1054e8fdf5401c7ab036c537f0d6114
Anton <tosha37@inbox.ru>
2019-03-16 11:40:33 +0300

    adding nugets

---
+++ ServerApp/ServerApp/CustomMiddleware/InitilizerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using FC_EMDB.Database.Initializer;
+using FC_EMDB.Database.UnitOfWork;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ServerApp.CustomMiddleware
+{
+    public class InitilizerMiddleware
+    {
+        private RequestDelegate _next;
+        public InitilizerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context,IUnitOfWork unitOfWork)
+        {
+            await _next(context);
+            DbInitializer.Seed(unitOfWork);
+           
+        }
+    }
+
+    public static class InitilizerMiddlewareExtentions
+    {
+        public static IApplicationBuilder UseEFCoreInitializer(this IApplicationBuilder builder)
+        {
+           return builder.UseMiddleware<InitilizerMiddleware>();
+        }
+    }
+}
