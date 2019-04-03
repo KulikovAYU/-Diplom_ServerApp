c5e8a915b7f1f7f8f2564e45f4d3f787fbd624ec
Anton <tosha37@inbox.ru>
2019-04-04 00:32:52 +0300

    added JSONCLient.cs

---
+++ ServerApp/JsonConverters/JSONEntities/JSONCLient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JsonConverters.JSONEntities
+{
+   public class JSONClient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } //имя
+
+        public string Family { get; set; } //фамилия
+
+        public string LastName { get; set; } //отчество
+
+        public DateTime DateOfBirdth { get; set; }//дата рождения
+
+        public int AbonementNumber { get; set; } //номер абонемента
+
+        public string AbonementType { get; set; } //тип абонемента
+
+        public string AbonementStatus { get; set; } //статус абонемента
+
+        public DateTime AbonementDaysFreezeCount { get; set; }//дни заморозки
+
+        public DateTime AbonementActionTime { get; set; } //дата действия абонемента
+
+        public DateTime AbonementEndTime { get; set; }//активно до
+
+        public bool IsAсtive { get; set; } //активен
+
+        public bool IsFreeze { get; set; } //заморожен
+    }
+}
